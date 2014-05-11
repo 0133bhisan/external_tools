db4ec4eb29aa4f18834d79d915c87f4a3a7c5394
chiposur <osurov314@gmail.com>
2014-05-11 16:16:08 -0400

    FINAL

--- FSdeletefile.java
+++ FSdeletefile.java
@@ -3,10 +3,12 @@ package external_tools;
 import java.io.*;
 
 public class FSdeletefile {
-    private int nullValue=0;
+    
+    private int nullValue = 0;
     private RandomAccessFile file;
     private long directoryBlockPos = 4096;
     private long currPositon = 0;
+
     public void start(String f, String filename) {
 	try {
 	    file = new RandomAccessFile(filename,"rw");
@@ -19,39 +21,38 @@ public class FSdeletefile {
     }
 
     private void deleteFile(String f) throws IOException {
-		long currPos = directoryBlockPos;
-		while ( currPos <= 8191 ) {
-		    file.seek(currPos);
-		    byte[] bytes = new byte[12];
-		    char[] chars = new char[12];
-		    int c = 0;
-		    while ( c < 12 ) {
-			bytes[c] = (byte)file.read();
-			chars[c] = (char)bytes[c];
-			if ( (int)chars[c] == 0 ) {
-			    break;
-			}
-			c++;
-		    }
-		    String s = "";
-		    c = 0;
-		    while ( c < 12 ) {
-			if ( (int)chars[c] == 0 ) {
-			    break;
-			}
-			s += chars[c];
-			c++;
-		    }
-		    if ( s.equals(f) ) {
-			currPos+=12;
-			//nullvalue
-			file.writeInt(nullValue);
-			return;
-		    }	
-		
-		    currPos += 16;
+	//A null value is 0x00000000
+	long currPos = directoryBlockPos;
+	while ( currPos <= 8191 ) {
+	    file.seek(currPos);
+	    byte[] bytes = new byte[12];
+	    char[] chars = new char[12];
+	    int c = 0;
+	    while ( c < 12 ) {
+		bytes[c] = (byte)file.read();
+		chars[c] = (char)bytes[c];
+		if ( (int)chars[c] == 0 ) {
+		    break;
 		}
-		throw new RuntimeException("File not found.");
+		c++;
+	    }
+	    String s = "";
+	    c = 0;
+	    while ( c < 12 ) {
+		if ( (int)chars[c] == 0 ) {
+		    break;
+		}
+		s += chars[c];
+		c++;
+	    }
+	    if ( s.equals(f) ) {
+		currPos+=12;
+		file.writeInt(nullValue);
+		return;
+	    }
+	    currPos += 16;
+	}
+	throw new RuntimeException("File not found.");
     }
 }
 
